6323a689aed6c209930b6b57663d77d2ff099b10
CaptDarkMatter <reederdt@gmail.com>
2020-01-05 09:01:38 -0800

    Recovery Commit 1
    A fatal bug appears when loading any version of the code from the MVP-Push branch after GUI Commit 2. The purpose of this branch is to recreate the now tainted code base and find the source of the error.

--- Otter Den/Scripts/Projectile.gd
+++ Otter Den/Scripts/Projectile.gd
@@ -1,7 +1,6 @@
 extends Area2D
 
 export (int) var speed
-export (int) var damage
 export (float) var lifetime
 
 var velocity = Vector2()
@@ -11,24 +10,20 @@ func start(_position, _direction):
 	rotation = _direction.angle()
 	$Lifetime.wait_time = lifetime
 	velocity = _direction * speed
-	pass
+	$Lifetime.start()
 
 func _process(delta):
 	position += velocity * delta
 
-func _ready():
-	pass
-
 func explode():
 	queue_free()
 
-func _on_Projectile_body_entered(body):
-	explode()
-	if body.has_method('take_damage'):
-		body.take_damage(damage)
-	pass # Replace with function body.
-
-
 func _on_Lifetime_timeout():
 	explode()
-	pass # Replace with function body.
+
+
+func _on_Projectile_area_entered(area):
+	explode()
+	var A_parent = area.get_parent()
+	if A_parent.has_method('take_damage'):
+		A_parent.take_damage()
--- Otter Den/Scripts/Tower.gd
+++ Otter Den/Scripts/Tower.gd
@@ -1,36 +1,54 @@
 extends Node2D
 
-signal shoot
+var projectile = preload("res://Scenes/Projectile.tscn")
+var targets = []
+var target_WF = []
+# warning-ignore:unused_class_variable
+var myProjectile : String 
 
-export (PackedScene) var projectile
+#signal shoot
+
 export (float) var gun_cooldown
 export (int) var detect_radius
 
 var can_shoot = true
+#onready var projectileSpawn = self.get_parent().get_node("../")
 
-func control(delta):
-	if Input.is_action_just_pressed("mouse_left"):
-		shoot()
+func _ready():
+	$GunTimer.wait_time = gun_cooldown
+	$DetectRadius/CollisionShape2D.shape.radius = detect_radius * 10
 
 func shoot():
 	if can_shoot:
 		can_shoot = false
 		$GunTimer.start()
 		var dir = Vector2(1,0).rotated($Turret.global_rotation)
-		emit_signal("shoot", projectile, $Turret/Muzzle.global_position, dir)
-		
-	pass
-
-func _ready():
-	$GunTimer.wait_time = gun_cooldown
-	$DetectRadius/CollisionShape2D.shape.radius = detect_radius
-	pass
+		var b = projectile.instance()
+		self.get_parent().add_child(b)
+		b.start($Turret/Muzzle.global_position, dir)
 
 func _on_GunTimer_timeout():
 	can_shoot = true
-	pass # Replace with function body.
-	
+
+# warning-ignore:unused_argument
 func _process(delta):
-	control(delta)
-	
-	
+	if targets.size() <= 0: 
+		return
+	if (!target_WF[0].get_ref()):
+		targets.remove(0)
+		target_WF.remove(0)
+	else:
+		self.look_at(targets[0].get_parent().position)
+		shoot()
+
+func _on_DetectRadius_area_entered(area):
+	var Apos = targets.size()
+	var TWFpos = target_WF.size()
+	if area.name == "Mob2D":
+		targets.insert(Apos, area) 
+		target_WF.insert(TWFpos, weakref(targets[targets.size() - 1].get_parent()))
+
+# warning-ignore:unused_argument
+func _on_DetectRadius_area_exited(area):
+	targets.remove(0)
+	target_WF.remove(0)
