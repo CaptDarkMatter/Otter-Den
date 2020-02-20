4b20c6c3b6e90dac1144bbd3c0fac3c03a872cc6
grboehringer <g2boehringer@gmail.com>
2020-02-19 23:45:11 -0600

    Forgot to save. Look at previous message.

--- Otter Den/Scripts/SaveGame.gd
+++ Otter Den/Scripts/SaveGame.gd
@@ -1,5 +1,9 @@
-extends Node
+# Saves and loads savegame files
+# Each node is responsible for finding itself in the save_game
+# dict so saves don't rely on the nodes' path or their source file
+extends Resource
 
+class_name SaveGame
 
-func _ready():
-	pass
+export var game_version : String = ''
+export var data : Dictionary = {}
--- Otter Den/Scripts/SaveLoad.gd
+++ Otter Den/Scripts/SaveLoad.gd
@@ -1,16 +1,11 @@
 extends Panel
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+onready var spin_box : SpinBox = $Column/HBoxContainer/SpinBox
+onready var game_saver : Node = $GameSaver
 
+func _on_SaveButton_pressed() -> void:
+	game_saver.save(spin_box.value)
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _on_LoadButton_pressed() -> void:
+	game_saver.load(spin_box.value)
